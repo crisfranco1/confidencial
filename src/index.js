3a628b13aa402498fbf9b2a29571c038baa878e4
Felipe Montoya <felmontoya@hugeinc.com>
2021-02-04 15:17:38 -0500

    Fixing bug in array of messages

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const messages = [
-    "😁 Sebastian",
-    "🤗 Johan 🤑",
-    "🖕 Felipe 🖕 te la hice 😂 🤣😅 😂 🤣",
-    "😁 Edgar 💩", 
-    "😈 Sanchez",
-    "😉 Kelly 🤩 ", 
-    "🤓 Eme 😝  ", 
-    "🤗 Jhon ⚽️ ", 
-    "😇 Franco"
+  "😁 Sebastian",
+  "🤗 Johan 🤑",
+  "🖕 Felipe 🖕 te la hice 😂 🤣😅 😂 🤣",
+  "😁 Edgar 💩",
+  "😈 Sanchez",
+  "😉 Kelly 🤩 ",
+  "🤓 Eme 😝  ",
+  "🤗 Jhon ⚽️ ",
+  "😇 Franco"
 ];
-  
+
 const randomMsg = () => {
     const message = messages[Math.floor(Math.random() * messages.length)];
     console.log(message);
   };
 
-  module.exports = { randomMsg };
+module.exports = { randomMsg };
